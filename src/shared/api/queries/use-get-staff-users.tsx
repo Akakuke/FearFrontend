7355570af81295a2ee46857e0f529e54b89dfa65
ayo <a2yomidiscord@gmail.com>
2025-04-15 21:51:02 +0300

    fix: cache key

--- src/shared/api/queries/use-get-staff-users.tsx
+++ src/shared/api/queries/use-get-staff-users.tsx
@@ -4,8 +4,7 @@ import { API_ENDPOINTS } from "../../../const/endpoints.ts";
 
 export function useGetStaffUsers(userId: string) {
   return useQuery({
-    queryKey: ["user_data"],
+    queryKey: ["user_data", userId],
     queryFn: () => api.get(API_ENDPOINTS.DISCORD_STAFF_USERS(userId)),
-    refetchInterval: 5000,
   });
 }
