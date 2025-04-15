c0aea46b6bcff514c623b771af79d879c43e1384
ayo <a2yomidiscord@gmail.com>
2025-04-15 22:12:39 +0300

    refactor: naming

--- src/shared/api/queries/use-get-staff-user.tsx
+++ src/shared/api/queries/use-get-staff-user.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { api } from "../utils/api.ts";
 import { API_ENDPOINTS } from "../../../const/endpoints.ts";
 
-export function useGetStaffUsers(userId: string) {
+export function useGetStaffUser(userId: string) {
   return useQuery({
     queryKey: ["user_data", userId],
     queryFn: () => api.get(API_ENDPOINTS.DISCORD_STAFF_USERS(userId)),
