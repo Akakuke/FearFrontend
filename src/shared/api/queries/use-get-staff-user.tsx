7c14e71776aa7a18dce0947b0ab419f56c5702ee
ayo <a2yomidiscord@gmail.com>
2025-04-15 22:15:18 +0300

    fix: tanstack query

--- src/shared/api/queries/use-get-staff-user.tsx
+++ src/shared/api/queries/use-get-staff-user.tsx
@@ -1,10 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
+import {useQuery, UseQueryOptions} from "@tanstack/react-query";
 import { api } from "../utils/api.ts";
 import { API_ENDPOINTS } from "../../../const/endpoints.ts";
+import {queryConfig} from "../types/queryConfig.ts";
 
-export function useGetStaffUser(userId: string) {
+export function useGetStaffUser(userId: string, options?: UseQueryOptions) {
   return useQuery({
     queryKey: ["user_data", userId],
     queryFn: () => api.get(API_ENDPOINTS.DISCORD_STAFF_USERS(userId)),
+    ...queryConfig,
+    ...options
   });
 }
---
+++ src/shared/api/types/queryConfig.ts
@@ -0,0 +1,9 @@
+const queryConfig = {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    retry: 0,
+    retryDelay: 5000,
+    staleTime: Infinity,
+};
+
+export { queryConfig };
